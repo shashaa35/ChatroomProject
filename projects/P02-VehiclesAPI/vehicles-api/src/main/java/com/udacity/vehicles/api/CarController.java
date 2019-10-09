393e7056a840d2d106eafeaa5cc95f25121003a7
shasagar <shashaa35@gmail.com>
2019-10-10 02:09:13 +0530

    adding changes for vehicle

--- projects/P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
+++ projects/P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
@@ -59,11 +59,11 @@ class CarController {
     @GetMapping("/{id}")
     Resource<Car> get(@PathVariable Long id) {
         /**
-         * TODO: Use the `findById` method from the Car Service to get car information.
-         * TODO: Use the `assembler` on that car and return the resulting output.
+         *  Use the `findById` method from the Car Service to get car information.
+         *  Use the `assembler` on that car and return the resulting output.
          *   Update the first line as part of the above implementing.
          */
-        return assembler.toResource(new Car());
+        return assembler.toResource(carService.findById(id));
     }
 
     /**
@@ -75,11 +75,11 @@ class CarController {
     @PostMapping
     ResponseEntity<?> post(@Valid @RequestBody Car car) throws URISyntaxException {
         /**
-         * TODO: Use the `save` method from the Car Service to save the input car.
-         * TODO: Use the `assembler` on that saved car and return as part of the response.
+         *  Use the `save` method from the Car Service to save the input car.
+         *  Use the `assembler` on that saved car and return as part of the response.
          *   Update the first line as part of the above implementing.
          */
-        Resource<Car> resource = assembler.toResource(new Car());
+        Resource<Car> resource = assembler.toResource(carService.save(car));
         return ResponseEntity.created(new URI(resource.getId().expand().getHref())).body(resource);
     }
 
@@ -92,12 +92,13 @@ class CarController {
     @PutMapping("/{id}")
     ResponseEntity<?> put(@PathVariable Long id, @Valid @RequestBody Car car) {
         /**
-         * TODO: Set the id of the input car object to the `id` input.
-         * TODO: Save the car using the `save` method from the Car service
-         * TODO: Use the `assembler` on that updated car and return as part of the response.
+         *  Set the id of the input car object to the `id` input.
+         *  Save the car using the `save` method from the Car service
+         *  Use the `assembler` on that updated car and return as part of the response.
          *   Update the first line as part of the above implementing.
          */
-        Resource<Car> resource = assembler.toResource(new Car());
+    	car.setId(id);
+        Resource<Car> resource = assembler.toResource(carService.save(car));
         return ResponseEntity.ok(resource);
     }
 
@@ -109,8 +110,9 @@ class CarController {
     @DeleteMapping("/{id}")
     ResponseEntity<?> delete(@PathVariable Long id) {
         /**
-         * TODO: Use the Car Service to delete the requested vehicle.
+         * Use the Car Service to delete the requested vehicle.
          */
+    	carService.delete(id);
         return ResponseEntity.noContent().build();
     }
 }
--- projects/P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
+++ projects/P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
@@ -1,8 +1,13 @@
 package com.udacity.vehicles.service;
 
+import com.udacity.vehicles.client.maps.MapsClient;
+import com.udacity.vehicles.client.prices.PriceClient;
 import com.udacity.vehicles.domain.car.Car;
 import com.udacity.vehicles.domain.car.CarRepository;
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,9 +20,15 @@ public class CarService {
 
     private final CarRepository repository;
 
+    @Autowired
+    PriceClient priceClient;
+    
+    @Autowired
+    MapsClient mapsClient;
+    
     public CarService(CarRepository repository) {
         /**
-         * TODO: Add the Maps and Pricing Web Clients you create
+         *  Add the Maps and Pricing Web Clients you create
          *   in `VehiclesApiApplication` as arguments and set them here.
          */
         this.repository = repository;
@@ -38,30 +49,31 @@ public class CarService {
      */
     public Car findById(Long id) {
         /**
-         * TODO: Find the car by ID from the `repository` if it exists.
+         *  Find the car by ID from the `repository` if it exists.
          *   If it does not exist, throw a CarNotFoundException
          *   Remove the below code as part of your implementation.
          */
-        Car car = new Car();
-
+    	Car car = repository.findById(id).orElseThrow(CarNotFoundException::new);
+    	        
+        
         /**
-         * TODO: Use the Pricing Web client you create in `VehiclesApiApplication`
+         *  Use the Pricing Web client you create in `VehiclesApiApplication`
          *   to get the price based on the `id` input'
-         * TODO: Set the price of the car
+         *  Set the price of the car
          * Note: The car class file uses @transient, meaning you will need to call
          *   the pricing service each time to get the price.
          */
 
-
+    	car.setPrice(priceClient.getPrice(id));
         /**
-         * TODO: Use the Maps Web client you create in `VehiclesApiApplication`
+         *  Use the Maps Web client you create in `VehiclesApiApplication`
          *   to get the address for the vehicle. You should access the location
          *   from the car object and feed it to the Maps service.
-         * TODO: Set the location of the vehicle, including the address information
+         *  Set the location of the vehicle, including the address information
          * Note: The Location class file also uses @transient for the address,
          * meaning the Maps service needs to be called each time for the address.
          */
-
+    	car.setLocation(mapsClient.getAddress(car.getLocation()));
 
         return car;
     }
@@ -90,15 +102,15 @@ public class CarService {
      */
     public void delete(Long id) {
         /**
-         * TODO: Find the car by ID from the `repository` if it exists.
+         *  Find the car by ID from the `repository` if it exists.
          *   If it does not exist, throw a CarNotFoundException
          */
-
+    	Car car = repository.findById(id).orElseThrow(CarNotFoundException::new);
 
         /**
-         * TODO: Delete the car from the repository.
+         *  Delete the car from the repository.
          */
-
+    	repository.delete(car);
 
     }
 }
